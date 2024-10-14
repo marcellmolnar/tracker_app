fa6533ac8537181e7dcd658068c42f17841f275b
Marcell Molnár <marcell.molnar97@gmail.com>
2024-10-14 20:25:35 +0200

    api added to get database values

---
+++ src/app/api/positions/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import sql from '@/lib/db.ts'
+
+export async function POST(req: NextRequest) {
+    return new NextResponse(JSON.stringify({ message: "Hello from Raspberry pi" }), { status: 200 });
+}
+
+export async function PUT(req: NextRequest) {
+    return new NextResponse(JSON.stringify({ message: "Hello from Raspberry pi" }), { status: 200 });
+}
+
+export async function GET(req: NextRequest) {
+    try {
+        const pos = await sql`SELECT * FROM positions LIMIT 30;`;
+        return new NextResponse({ positions: pos });
+    } catch (error) {
+    console.error('Error in POST handler:', error);
+        return new NextResponse(JSON.stringify({ error: 'Error processing request' }), { status: 500 });
+    }
+}
